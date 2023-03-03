d9954c33d32771431d697794a0751409b851f7b9
penscola <penscolashackletonfelix@gmail.com>
2023-03-03 17:05:45 +0300

    AvaxGods Sols

---
+++ client/src/contract/index.js
@@ -0,0 +1,4 @@
+import contract from './AVAXGods.json';
+
+export const ADDRESS = '0xd45e40444152bB158d857248D8e952742a4413F4'
+export const { abi: ABI } = contract;
---
+++ client/src/page/Home.jsx
@@ -0,0 +1,40 @@
+import { constants } from 'ethers';
+import React, { useState } from 'react';
+
+import { PageHOC, CustomInput, CustomButton } from '../components';
+import { useGlobalContext } from '../context';
+
+const Home = () => {
+  const { contract, walletAddress } = useGlobalContext();
+  const [ playerName, setPlayerName ] = useState('');
+
+  const handleClick = async () => {
+    try {
+      const PLayerExists = await contract.isPlayer(walletAddress);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  return (
+    <div className="flex flex-col">
+      <CustomInput 
+        label = "Name"
+        placeholder = "Enter your player name"
+        value = {playerName}
+        handleValueChange = {setPlayerName}
+      />
+      <CustomButton 
+            title="Register"
+            handleClick={(handleClick) => {}}
+            restStyles = "mt-6"
+        />
+    </div>
+  )
+};
+
+export default PageHOC(
+  Home,
+  <>Welcome to Avax Gods <br /> a Web3 NFT Card Game</>,
+  <>Connect your wallet to start playing <br /> the ultimate Web3 Battle Card Game</>
+);
