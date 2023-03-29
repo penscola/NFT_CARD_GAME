e0be2f201631a3e1e307d02c441bb73f979db794
penscola <penscolashackletonfelix@gmail.com>
2023-03-29 14:42:04 +0300

    added address

--- client/src/contract/index.js
+++ client/src/contract/index.js
@@ -1,4 +1,4 @@
 import contract from './AVAXGods.json';
 
-export const ADDRESS = '0xC6825E381F728a0300f3FD1bf82d9B378FFD83eA';
+export const ADDRESS = '0x5227d89Aff11D43F4DD1B4444e052Bd3600AB70A';
 export const { abi: ABI } = contract;
